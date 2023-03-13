a293773eccc33bdc4784fbd35e59961cf05fc204
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-03-13 14:58:51 -0300

    cliente model added

--- src/main/java/br/com/banco/model/Cliente.java
+++ src/main/java/br/com/banco/model/Cliente.java
@@ -1,5 +1,61 @@
 package br.com.banco.model;
 
-public class Cliente {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tbl_cliente")
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private int id_cliente;
+	
+	@Column(name = "nome_cliente", length = 100, nullable = false)
+	private String nome_cliente;
+	
+	@Column(name = "cpf_cliente", length = 20, nullable = false)
+	private String cpf_cliente;
+	
+	@Column(name = "telefone_cliente", length = 20, nullable = false, unique = true)
+	private String telefone_cliente;
+
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome_cliente() {
+		return nome_cliente;
+	}
+
+	public void setNome_cliente(String nome_cliente) {
+		this.nome_cliente = nome_cliente;
+	}
+
+	public String getCpf_cliente() {
+		return cpf_cliente;
+	}
+
+	public void setCpf_cliente(String cpf_cliente) {
+		this.cpf_cliente = cpf_cliente;
+	}
+
+	public String getTelefone_cliente() {
+		return telefone_cliente;
+	}
+
+	public void setTelefone_cliente(String telefone_cliente) {
+		this.telefone_cliente = telefone_cliente;
+	}
+	
+	
+	
 }
