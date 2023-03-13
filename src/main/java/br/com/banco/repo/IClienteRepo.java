f62a8780b47519c912d562c362e7f34323e369ae
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-03-13 17:14:32 -0300

    arrumando o arquivo conta

--- src/main/java/br/com/banco/repo/IClienteRepo.java
+++ src/main/java/br/com/banco/repo/IClienteRepo.java
@@ -1,13 +1,9 @@
 package br.com.banco.repo;
 
-<<<<<<< HEAD
-public interface IClienteRepo {
-=======
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.banco.model.Cliente;
 
 public interface IClienteRepo extends CrudRepository<Cliente, Integer> {
->>>>>>> 1add1bc0351e36e77bf264fb8b4c7dfe76ced138
 
 }
