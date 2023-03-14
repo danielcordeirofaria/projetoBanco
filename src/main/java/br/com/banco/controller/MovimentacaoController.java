fd09b690b31cd22b2837b706ac520b327f01ff25
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-03-14 15:33:30 -0300

    atualizando MovimentacaoController

--- src/main/java/br/com/banco/controller/MovimentacaoController.java
+++ src/main/java/br/com/banco/controller/MovimentacaoController.java
@@ -1,5 +1,35 @@
 package br.com.banco.controller;
 
-public class MovimentacaoController {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.banco.dto.MensagemErro;
+import br.com.banco.model.Movimentacao;
+import br.com.banco.service.IMovimentacaoService;
+
+@RestController
+public class MovimentacaoController {
+	
+	@Autowired
+	IMovimentacaoService service;
+	
+	@PostMapping("/movimentacao")
+	public ResponseEntity<?> cadastrarMovimentacao(@RequestBody Movimentacao m){
+		Movimentacao res = service.cadastrarMovimentacao(m);
+		if(res != null) {
+			return ResponseEntity.status(201).body(res);
+		}
+		return ResponseEntity.status(400).body(new MensagemErro(400, "Operação não realizada"));
+	}
+	
+	@GetMapping("/movimentacao/{id}")
+	public ArrayList<Movimentacao> recuperarTodas(@RequestBody Movimentacao m){
+		
+	}
 }
