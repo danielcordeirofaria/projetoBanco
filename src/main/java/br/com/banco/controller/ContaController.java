4e83808988be73466b99ae74767ef72e9d53faa8
AndreAugustoR <andreaugustoramos@gmail.com>
2023-03-15 15:04:49 -0300

    Att ContaController 2

--- src/main/java/br/com/banco/controller/ContaController.java
+++ src/main/java/br/com/banco/controller/ContaController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.dto.MensagemErro;
+import br.com.banco.model.Cliente;
 import br.com.banco.model.Conta;
 import br.com.banco.service.IContaService;
 
@@ -35,5 +38,14 @@ public class ContaController {
 			return ResponseEntity.ok(c);
 		}
 		return ResponseEntity.status(404).body(new MensagemErro(404, "Número não localizado!"));
+	}
+	
+	@PostMapping("/contas")
+	public ResponseEntity<?> adicionarConta(@RequestBody Conta c){
+		Conta res = service.adicionarContas(c);
+		if(res != null) {
+			return ResponseEntity.status(201).body(res);
+		}
+		return ResponseEntity.status(400).body(new MensagemErro(400, "Conta não cadastrada"));
 	}
 }
